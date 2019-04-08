5222edc1813d1223178ca732dc9cb58f46ada24f
Erick Enriquez <eenriqu5@asu.edu>
2019-04-07 18:43:06 -0700

    lesson 2 layout design

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-import tkinter as tk #importing library
+from tkinter import * #importing library and making a reference
 
 
-win = tk.Tk() #creates our window
+win = Tk() #creates an instance of our window
 
-win.title("CSE 355 Extra Credit")
+#theLabel = Label(win,text = "this is too easy")
+
+#theLabel.pack()# lazy way , just packs the label
 
 
-win.mainloop()#event loop will end when you hit x
+topFrame = Frame(win)
+topFrame.pack(side  = TOP )
+bottomFrame = Frame(win)
+bottomFrame.pack(side = BOTTOM)#packs the bottom frame on the bottom
+
+
+#button_1 = Button(topFrame,text = "BUTTON1" , fg ="red")
+#button_2 = Button(topFrame,text = "BUTTON2" , fg ="blue")
+#button_3 = Button(topFrame,text = "BUTTON3" , fg ="red")
+#button_4 = Button(bottomFrame,text = "BUTTON4" , bg ="yellow")
+
+#button_1.pack(side = "left")
+#button_2.pack(side  = "right")
+#button_3.pack()
+#button_4.pack()
+ 
+
+win.mainloop()#runs the code
