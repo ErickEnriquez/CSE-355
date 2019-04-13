12f2c242d1a02656e8a12da2ee8507dcd2df43b5
Erick Enriquez <eenriqu5@asu.edu>
2019-04-12 18:45:25 -0700

    Gui skeleton starting to take shape

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 from tkinter import * #importing library and making a reference
 
-win = Tk() #creates an instance of our window
+root = Tk()
 
 
-def printword():
-    print('using command')
-
-def printw(event):
-    print('using bind')
-
-button2 = Button(win,text = "press me" )
-button2.bind("<Button-1>" , printw)
-button2.pack()
-
-b  = Button(win , text  ="print name" , command=printword)
-b.pack()
+title  = Label(root , text=  "DFA for langauge that must start and end with the same symbol\nTest your String below")
+title.grid(row = 0)
+entryString = Entry(root )
+entryString.grid(row = 1 , column  = 0)
+button = Button(root, text = "Submit")
+button.grid(row = 2 ,column =0)
+canvas = Canvas(root, width =500 ,height = 500)
+canvas.grid(row = 1 ,column = 1)
 
 
 
+state0 = canvas.create_oval(300,20,360,80)
+state1 = canvas.create_oval(200,150,260,210)#accept state
+accept_s1 = canvas.create_oval(190,140,270,220)# double cirle
+state2 = canvas.create_oval(400,150,460,210)#accept state
+accept_s1 = canvas.create_oval(390,140,470,220)#double circle
+state3 = canvas.create_oval(200,330,260,390)
+state4 = canvas.create_oval(400,330,460,390)
+s0_label = canvas.create_text( 330,50 , text = "s0")
+s1_label = canvas.create_text(230,180,text = "s1")
+s2_label = canvas.create_text(430,180,text = "s2")
+s3_label = canvas.create_text(230,360,text = "s3")
+s4_label =canvas.create_text(430,360,text ="s4")
 
-win.mainloop()#runs the code
+
+
+root.mainloop()#runs the code
 
 
 
@@ -48,3 +58,20 @@ win.mainloop()#runs the code
 
 #checkBox = Checkbutton(win,text = "keep me signed in")#check button
 #checkBox.grid( columnspan =2)
+
+
+#/////////////////////////////////////////////////////////////
+
+
+#f =  Frame(win, width="500",height="500")
+
+#def right(event):
+ #   print("Right")
+
+#def left(event):
+ #   print("Right")
+
+
+#f.bind("<Button-1>", left)
+#f.bind("<Button-3>" , right)
+#f.pack()
